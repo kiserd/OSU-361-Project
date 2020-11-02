3ab52a7f13301bc43a8b33707f388bb6e2a0fbe1
kiserd <59572911+kiserd@users.noreply.github.com>
2020-11-02 09:30:30 -0800

    added dbcon for mongo sample data

---
+++ mongodbcon.js
@@ -0,0 +1,43 @@
+const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://kiserd:Qddegw7G5aRv4zpG@cluster0.uyadt.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri);
+
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://kiserd:ntTnMUdRWyfn9uqc@cluster0.uyadt.mongodb.net/test?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+
+/**
+ * Print the names of all available databases
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ */
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
