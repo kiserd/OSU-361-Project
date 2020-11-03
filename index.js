00f03dff34e253c208600e4a361d0e76937866b2
kiserd <59572911+kiserd@users.noreply.github.com>
2020-11-02 16:57:26 -0800

    more handlebars testing

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
 const express = require('express');
-//const cors = require('cors');
 const path = require('path');
 const handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 const PORT = process.env.PORT || 5000;
 const app = express();
 const ingredients = require('./ingredients.json');
 const recipes = require('./recipes.json');
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://kiserd:ntTnMUdRWyfn9uqc@cluster0.uyadt.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri);
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-//app.use(cors());
-//app.use('/graphql', graphqlHTTP({schema, rootValue}));
-//app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
-
-// console.log(ingredients);
-// console.log(recipes);
 
 app.get('/', (req , res, next) => {
   var context = {};
-  context['ingredients'] = []
+  context['recipes'] = []
   for (var i = 0; i < recipes.length; i++) {
-    context['ingredients'].push(recipes[i]["name"]);
+    context['recipes'].push(recipes[i]["name"]);
   }
   console.log(context);
   res.render('choose_recipe', context);
