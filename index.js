b86efabffc35b07c90d05796246802c4cee442ed
Victoria Dmyterko <victoriadmyterko@gmail.com>
2020-11-07 18:25:46 -0800

    Recipebook work

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ app.get('/my_recipes', (req , res, next) => {
     //   arrayOfObjects.savedRecipes.push(addRecipe);
     })
 
-    //console.log(arrayOfObjects)
+    console.log(arrayOfObjects)
 
     // fs.writeFile('./myRecipes.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
     //   if (err) throw err
