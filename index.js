250ffe897c11e67857ac6d1a220ec51e4e0ceda1
kiserd <59572911+kiserd@users.noreply.github.com>
2020-11-07 16:36:17 -0800

    put ingredients/recipes data in index.js to troubleshoot

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path');
 const handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 const PORT = process.env.PORT || 5000;
 const app = express();
-var ingredients = require('./ingredients.json');
-var recipes = require('./recipes.json');
+// var ingredients = require('./ingredients.json');
+// var recipes = require('./recipes.json');
 
 
 app.engine('handlebars', handlebars.engine);
@@ -12,6 +12,44 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+
+// main recipes 'database' :P
+var recipes = [
+    {"type": "breakfast", "userRecipe": false, "name": "Oatmeal", "ingredients": ["oats", "milk", "cranberries", "cinnamon", "sugar"]},
+    {"type": "breakfast", "userRecipe": false, "name": "Scrambled Eggs", "ingredients": ["egg", "cheese", "bell pepper"]},
+    {"type": "breakfast", "userRecipe": false, "name": "Breakfast Burrito", "ingredients": ["Tortilla", "Black Beans", "Eggs", "Onion", "Potato", "Sour Cream", "Salsa", "Avocado"]},
+    {"type": "lunch", "userRecipe": false, "name": "Bean Burrito", "ingredients": ["beans", "cheese", "rice", "tortilla", "salsa"]},
+    {"type": "lunch", "userRecipe": false, "name": "Chicken Teriyaki", "ingredients": ["rice", "chicken", "teriyaki sauce"]},
+    {"type": "lunch", "userRecipe": false, "name": "Turkey Sandwich", "ingredients": ["turkey", "bread", "cheese", "mayonaise", "lettuce", "tomato"]},
+    {"type": "lunch", "userRecipe": false, "name": "Waldorf Salad", "ingredients": ["mayonaise", "apples", "celery", "walnuts", "grapes", "Lemon", "lettuce"]},
+    {"type": "lunch", "userRecipe": false, "name": "Pumpkin Soup", "ingredients": ["pumpkin", "olive oil", "onion", "vegetable broth", "coconut milk", "pumpkin seeds"]},
+    {"type": "lunch", "userRecipe": false, "name": "Chicken Caesar Wrap", "ingredients": ["tortilla", "chicken", "lettuce", "tomato", "cheese", "caesar dressing", "crouton"]},
+    {"type": "dinner", "userRecipe": false, "name": "Pepperoni Pizza", "ingredients": ["pork", "flour", "cheese", "tomato sauce"]},
+    {"type": "dinner", "userRecipe": false, "name": "Cheese Pizza", "ingredients": ["flour", "cheese", "tomato sauce"]},
+    {"type": "dinner", "userRecipe": false, "name": "Hamburger", "ingredients": ["ground beef", "ketchup", "bread", "lettuce", "tomato"]},
+    {"type": "dinner", "userRecipe": false, "name": "Cheeseburger", "ingredients": ["ground beef", "ketchup", "bread", "lettuce", "tomato", "cheese"]},
+    {"type": "dinner", "userRecipe": false, "name": "Mac and Cheese", "ingredients": ["pasta", "milk", "butter", "cheese"]},
+    {"type": "dinner", "userRecipe": false, "name": "Spaghetti", "ingredients": ["pasta", "tomato sauce", "ground beef"]},
+    {"type": "dinner", "userRecipe": false, "name": "Mostaccioli", "ingredients": ["pasta", "cheese", "egg", "pork", "tomato sauce"]},
+    {"type": "dinner", "userRecipe": false, "name": "Meatloaf", "ingredients": ["ground beef", "egg", "ketchup", "Worcestshire Sauce", "Dijon Mustard", "onion", "breadcrumbs", "brown sugar"]}
+]
+
+// main ingredients 'database' :P
+var ingredients = [
+  {"type": "meat", "name": "ground beef", "impact": 13021},
+  {"type": "meat", "name": "chicken", "impact": 2868},
+  {"type": "meat", "name": "pork", "impact": 5736},
+  {"type": "bread", "name": "pasta", "impact": 616},
+  {"type": "bread", "name": "bread", "impact": 536},
+  {"type": "bread", "name": "rice", "impact": 832},
+  {"type": "vegetable", "name": "lettuce", "impact": 133},
+  {"type": "vegetable", "name": "tomato", "impact": 108},
+  {"type": "dairy", "name": "cheese", "impact": 3153},
+  {"type": "dairy", "name": "butter", "impact": 7193},
+  {"type": "dairy", "name": "egg", "impact": 2017},
+  {"type": "sauce", "name": "ketchup", "impact": 178},
+  {"type": "sauce", "name": "tomato sauce", "impact": 285}
+]
 
 function getImpactByRecipeIngredient(recipe) {
   var impacts = [];
