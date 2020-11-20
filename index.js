15ba865df9a0df9f6b7dc7142b5d54956a340f1a
kiserd <59572911+kiserd@users.noreply.github.com>
2020-11-20 14:13:49 -0800

    complete shot in the dark

--- index.js
+++ index.js
@@ -328,7 +328,7 @@ app.get('/view_ingredients', async (req , res, next) => {
   context = {}
   context["ingredients"] = ingredients;
   context["recipe"] = recipes[0];
-  res.render('view_ingredients', context);
+  res.render('view_ingredients', context, recipes, ingredients);
 });
 
 app.get('/view_substitutes', (req, res, next) => {
